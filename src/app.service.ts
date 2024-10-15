7742b37fbb065b6bc25976dc1e4fd1240bb2be50
Diego Sánchez Dueñas <diego.sanchez@estevezjor.mx>
2024-10-15 10:45:44 -0600

    last test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Estevez de nuevo!';
+    return 'Hello World Estevez de nuevo, desde Github Action!';
   }
 }
